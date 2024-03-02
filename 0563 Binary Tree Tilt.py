183d31838ef0a2672038331023b607e9d480e2db
Savar Jain <jainsavar@gmail.com>
2024-03-02 18:13:05 -0500

    Create 0563 Binary Tree Tilt.py

---
+++ 0563 Binary Tree Tilt.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def dfs(node):
+            nonlocal res
+            if not node: return 0
+            left, right = dfs(node.left), dfs(node.right)
+            res += abs(left-right)
+            return left+right+node.val
+        
+        dfs(root)
+        return res
