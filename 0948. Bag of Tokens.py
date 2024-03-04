098eb8da99d8c90a2f80047a6c83a8dddcc6ee59
Savar Jain <jainsavar@gmail.com>
2024-03-03 20:52:17 -0500

    Create 0948. Bag of Tokens.py

---
+++ 0948. Bag of Tokens.py
@@ -0,0 +1,17 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        l, r = 0, len(tokens)-1
+        score = 0
+        while l <= r:
+            if power >= tokens[l]:
+                score += 1
+                power -= tokens[l]
+                l += 1
+            elif score >= 1 and l != r:
+                score -= 1
+                power += tokens[r]
+                r -= 1
+            else: break
+        
+        return score
