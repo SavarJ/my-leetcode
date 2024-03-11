777efb951731b2be5ba68ba1647f80092b703ce5
Savar Jain <jainsavar@gmail.com>
2024-03-10 20:45:18 -0400

    Create 0270. Closest Binary Search Tree Value.py

---
+++ 0270. Closest Binary Search Tree Value.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        res = inf
+        while root:
+            res = min(res, root.val, key=lambda x: (abs(target-x), x))
+            root = root.left if target < root.val else root.right
+        return res
