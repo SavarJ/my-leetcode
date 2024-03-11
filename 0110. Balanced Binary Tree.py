b52d47f8b3171f0e0bf49ca6d4c8eeb79ee620ef
Savar Jain <jainsavar@gmail.com>
2024-03-11 17:36:07 -0400

    Create 0110. Balanced Binary Tree.py

---
+++ 0110. Balanced Binary Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.res = True
+        def dfs(node):
+            if not node: return 0
+            left, right = dfs(node.left), dfs(node.right)
+            if abs(left-right) > 1: self.res = False
+            return max(left, right)+1
+        
+        dfs(root)
+        return self.res
