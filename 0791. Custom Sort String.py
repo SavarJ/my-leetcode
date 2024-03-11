1ab70788239b958f48b0aed40c1a928475153f97
Savar Jain <jainsavar@gmail.com>
2024-03-11 14:03:06 -0400

    Update 0791. Custom Sort String.py

--- 0791. Custom Sort String.py
+++ 0791. Custom Sort String.py
@@ -1,3 +1,3 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        return [freq:=Counter(s), ''.join([c*freq[c] for c in order] + [c*freq[c] for c in set(s).difference(set(order))])][1]
+        return [freq:=Counter(s), ''.join([c*freq[c] for c in sorted(ascii_lowercase, key=order.find)])][1]
