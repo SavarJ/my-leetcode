506d3795eeadad7f849c757af118e22244d88da9
Savar Jain <jainsavar@gmail.com>
2024-04-14 20:14:03 -0400

    Create 0129. Sum Root to Leaf Numbers.py

---
+++ 0129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, num):
+            if not node: return 0
+            num = num*10+node.val
+            if not (node.left or node.right): return num
+            return dfs(node.left, num) + dfs(node.right, num)
+        return dfs(root, 0)
