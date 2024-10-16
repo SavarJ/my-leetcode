26c24b7f283012f09f8e429987b58d2195e3e3ac
Savar Jain <jainsavar@gmail.com>
2024-10-16 16:33:37 -0400

    Update 0322. Coin Change.py

--- 0322. Coin Change.py
+++ 0322. Coin Change.py
@@ -1,10 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0]+[inf]*(amount)
-        for i in range(amount+1):
+        dp = [0] + [inf] * (amount)
+        for total in range(1, amount+1):
             for coin in coins:
-                dp[i] = min(dp[i], inf if i-coin<0 else dp[i-coin]+1)
-        return -1 if dp[-1] == inf else dp[-1]
+                if total-coin >= 0:
+                    dp[total] = min(dp[total], dp[total-coin]+1)
+        
+        return dp[amount] if dp[amount] != inf else -1
+        
+        # dp = [0]+[inf]*(amount)
+        # for i in range(amount+1):
+        #     for coin in coins:
+        #         dp[i] = min(dp[i], inf if i-coin<0 else dp[i-coin]+1)
+        # return -1 if dp[-1] == inf else dp[-1]
 
         # cache = {0:0}
         # def bt(total):
