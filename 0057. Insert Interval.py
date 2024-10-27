ff7ab4d77c961cbf3a59402441227453afea2e25
Savar Jain <jainsavar@gmail.com>
2024-10-27 17:43:12 -0400

    Update 0057. Insert Interval.py

--- 0057. Insert Interval.py
+++ 0057. Insert Interval.py
@@ -1,14 +1,12 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        for i in reversed(range(-1, len(intervals))):
-            if i == -1: intervals.insert(0, newInterval)
-            elif newInterval[0] >= intervals[i][0]:
-                intervals.insert(i+1, newInterval)
-                break
+        res = []
+        for i, interval in enumerate(intervals):
+            if newInterval[1] < interval[0]: # new interval finishes before
+                return res + [newInterval] + intervals[i:]
+            elif newInterval[0] > interval[1]: # new interval starts after
+                res.append(interval)
+            else:
+                newInterval = [min(newInterval[0], interval[0]), max(newInterval[1], interval[1])]
         
-        stack = [intervals[0]]
-        for i in range(1, len(intervals)):
-            a, b = stack.pop(), intervals[i]
-            stack.extend([a, b] if b[0] > a[1] else [[min(a[0],b[0]), max(a[1],b[1])]])
-        
-        return stack
+        return res + [newInterval]
