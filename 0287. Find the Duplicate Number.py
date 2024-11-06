0894e32854e2df096196f924cac1a7a71d902e1d
Savar Jain <jainsavar@gmail.com>
2024-11-05 21:03:22 -0500

    Update 0287. Find the Duplicate Number.py

--- 0287. Find the Duplicate Number.py
+++ 0287. Find the Duplicate Number.py
@@ -1,6 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # nums.sort()
+        # for i,n in enumerate(nums[1:]):
+        #     if n == nums[i]:
+        #         return n
+
         slow = again = fast = 0
-        while slow == 0 or slow != fast: slow, fast = nums[slow], nums[nums[fast]]
-        while slow != again: slow, again = nums[slow], nums[again]
+        while True:
+            slow, fast = nums[slow], nums[nums[fast]]
+            if slow == fast: break
+
+        while slow != again: 
+            slow, again = nums[slow], nums[again]
+            
         return again
